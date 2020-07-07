6eae2520ae0ac25ac9f135718f998b358a0ca957
gabaaa <not-set>
2020-07-06 22:13:53 -0500

    added files

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# authors: Gabriel Auger
+# name: gitlib
+# licenses: MIT 
+
+__version__ = "0.2.0"
+
+# from .dev.bump_version import bump_version
+# from .gpkgs import message as msg
+
